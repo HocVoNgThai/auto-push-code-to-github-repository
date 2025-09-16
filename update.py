a4ed8659e830a45ae64b353b193510241ad40b1b
HocVoNgThai <22520489@gm.uit.edu.vn>
2025-09-16 16:34:37 +0700

    Auto update 2025-09-16 16:34:37

--- update.py
+++ update.py
@@ -5,11 +5,17 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class GitHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.last_event = 0
+        self.debounce = 10
+
     def on_any_event(self, event):
-        if not event.is_directory:
-            subprocess.run(['git', 'add', '.'])
-            subprocess.run(['git', 'commit', '-m', f'Auto update {time.strftime("%Y-%m-%d %H:%M:%S")}'])
-            subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+        if not event.is_directory and time.time() - self.last_event > self.debounce:
+            self.last_event = time.time()
+            if subprocess.run(['git', 'status', '--porcelain']).stdout:
+                subprocess.run(['git', 'add', '.'])
+                subprocess.run(['git', 'commit', '-m', f'Auto update {time.strftime("%Y-%m-%d %H:%M:%S")}'])
+                subprocess.run(['git', 'push', 'u','origin', 'main'])
 
 if __name__ == "__main__":
     event_handler = GitHandler()
